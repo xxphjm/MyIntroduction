61c71004ed003b538add84a1b015a8fdd29ae509
hjm <71825356+xxphjm@users.noreply.github.com>
2023-12-13 10:31:17 +0800

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -125,7 +125,7 @@
 			$titleBar = $(
 				'<div id="titleBar">' +
 					'<a href="#header" class="toggle"></a>' +
-					'<span class="title">' + $('#logo').html() + '</span>' +
+					'<span class="title">履歷表</span>' +
 				'</div>'
 			)
 				.appendTo($body);
